e7a2ce8e50a4c208408cc7429046fb9bd0baf086
eun417 <126125547+eun417@users.noreply.github.com>
2024-01-30 17:05:27 +0900

    setting: 프로젝트 세팅

---
+++ src/test/java/com/chungjin/wam/WamApplicationTests.java
@@ -0,0 +1,13 @@
+package com.chungjin.wam;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WamApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
