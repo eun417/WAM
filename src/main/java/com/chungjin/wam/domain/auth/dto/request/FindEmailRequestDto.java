eaf880c4c29703d272435ea0219b1891f58eb30d
eun417 <126125547+eun417@users.noreply.github.com>
2024-02-09 19:17:36 +0900

    Refactor: 휴대폰 번호, 비밀번호 정규식 상수화

--- src/main/java/com/chungjin/wam/domain/auth/dto/request/FindEmailRequestDto.java
+++ src/main/java/com/chungjin/wam/domain/auth/dto/request/FindEmailRequestDto.java
@@ -6,13 +6,15 @@ import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
 
+import static com.chungjin.wam.global.util.RegExp.PHONE_NUMBER_REGEXP;
+
 @Getter
 @Setter
 @NoArgsConstructor
 public class FindEmailRequestDto {
 
-    @NotBlank(message = "전화번호를 입력해주세요.")
-    @Pattern(regexp = "^01(?:0|1|[6-9])[.-]?(\\d{3}|\\d{4})[.-]?(\\d{4})$", message = "10 ~ 11 자리의 숫자만 입력 가능합니다.")
+    @NotBlank(message = "휴대폰 번호를 입력해주세요.")
+    @Pattern(regexp = PHONE_NUMBER_REGEXP, message = "휴대폰 번호는 10 ~ 11 자리의 숫자만 입력 가능합니다.")
     private String phoneNumber;
 
     @NotBlank
