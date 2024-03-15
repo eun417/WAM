ff09525df306efaa93350b7e621ca573398a628a
eun417 <eunahchung0417@gmail.com>
2024-03-15 22:47:28 +0900

    fix: CustomException null 에러 수정
    - null이 아닌 에러 메시지 출력

--- src/main/java/com/chungjin/wam/global/exception/CustomException.java
+++ src/main/java/com/chungjin/wam/global/exception/CustomException.java
@@ -1,13 +1,19 @@
 package com.chungjin.wam.global.exception;
 
+import com.chungjin.wam.global.exception.error.ErrorCode;
 import com.chungjin.wam.global.exception.error.ErrorCodeType;
+import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.RequiredArgsConstructor;
 
 @Getter
-@RequiredArgsConstructor
 public class CustomException extends RuntimeException {
 
-    private final ErrorCodeType errorCode;
+    private final ErrorCode errorCode;
+
+    public CustomException(ErrorCode errorCode) {
+        super(errorCode.getMessage());   //CustomException이 발생할 때 ErrorCode에 정의된 메시지로 설정
+        this.errorCode = errorCode;
+    }
 
 }
