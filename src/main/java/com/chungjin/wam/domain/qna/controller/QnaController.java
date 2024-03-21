5afecdd598e0a55e73a6bd875486920ed00a8c0b
eun417 <eunahchung0417@gmail.com>
2024-03-21 17:07:59 +0900

    style: QnA 웹 퍼블리싱 수정, 뷰 컨트롤러 작성
    +) 기타 등등 수정

---
+++ src/main/java/com/chungjin/wam/domain/qna/controller/QnaController.java
@@ -0,0 +1,42 @@
+package com.chungjin.wam.domain.qna.controller;
+
+import com.chungjin.wam.domain.auth.dto.CustomUserDetails;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Slf4j
+@Controller
+@RequestMapping("/qna")
+public class QnaController {
+
+    /**
+     * QnA 목록 조회
+     */
+    @GetMapping("/list")
+    public String goQnaList() {
+        return "qna/qnaList";
+    }
+
+    /**
+     * QnA 상세 조회
+     */
+    @GetMapping("/detail/{qnaId}")
+    public String goQnaDetail(@PathVariable("qnaId") Long qnaId, Model model) {
+        model.addAttribute("qnaId", qnaId);
+        return "qna/qnaDetail";
+    }
+
+    /**
+     * QnA 작성폼
+     */
+    @GetMapping("/write")
+    public String goQnaWrite() {
+        return "qna/qnaWrite";
+    }
+
+}
