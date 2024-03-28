8cb8d9350560b495edbce54d28e48315b8f70d3b
eun417 <eunahchung0417@gmail.com>
2024-03-28 23:46:11 +0900

    rename: header, footer를 common 패키지로 이동
    +) footer 바닥 고정

--- src/main/resources/static/js/common/checkToken.js
+++ src/main/resources/static/js/common/checkToken.js
@@ -1,8 +1,3 @@
-//로컬 스토리지에서 토큰을 가져오는 함수
-function getToken() {
-    return localStorage.getItem('accessToken');
-}
-
 //토큰을 토컬 스토리지에 저장
 function saveTokenToLocalStorage(accessToken, refreshToken) {
     localStorage.setItem('accessToken', accessToken);
@@ -10,24 +5,21 @@ function saveTokenToLocalStorage(accessToken, refreshToken) {
 }
 
 //토큰을 토컬 스토리지에서 삭제
-function removeTokenInLocalStorage(accessToken, refreshToken) {
+function removeTokenInLocalStorage() {
     localStorage.removeItem('accessToken');
     localStorage.removeItem('refreshToken');
 }
 
 //payload에서 데이터 가져오기
-function getPayloadData() {
-    const token = getToken();
-
-    //토큰이 없는 경우 0 반환
-    if (!token) {
-        return 0;
+function getPayloadData(accessToken) {
+    //토큰이 없는 경우 null 반환
+    if (!accessToken) {
+        return null;
     }
     //토큰의 payload 부분 추출
     var base64Payload = token.split('.')[1];    //value 0: header, 1: payload, 2: VERIFY SIGNATURE
     //base64 디코딩 및 JSON 객체로 변환
-    const payload = JSON.parse(atob(base64Payload.replace(/-/g, '+').replace(/_/g, '/')));
-    return payload;
+    return JSON.parse(atob(base64Payload.replace(/-/g, '+').replace(/_/g, '/')));
 }
 
 //토큰 만료 시간 확인
@@ -44,6 +36,12 @@ function getTokenExpirationDate(exp) {
 
 //새로운 access token을 요청하는 함수
 function refreshAccessToken() {
+    const refreshToken = localStorage.getItem('refreshToken');
+
+    if (!refreshToken) {
+        return Promise.reject('Refresh token을 찾을 수 없습니다.');
+    }
+
     //TokenRequestDto 객체 생성
     const tokenReq = {
         refreshToken: localStorage.getItem('refreshToken')
@@ -52,23 +50,24 @@ function refreshAccessToken() {
     //refresh 요청
     return axios.post('/auth/refresh', tokenReq)
         .then(function(response) {
+            console.log('Access token 재발급 성공');
             //새로운 access token 발급
             const { accessToken } = response.data;
             localStorage.setItem('accessToken', accessToken);
         })
         .catch(function(error) {
-            console.error('새로운 access token 발급 실패:', error);
-            localStorage.removeItem('refreshToken');
+            console.error('Access token 재발급 실패:', error);
+            removeTokenInLocalStorage();
         });
 }
 
 /*토큰 만료 여부 검사, refresh 요청 함수*/
 function checkTokenExpiration() {
-    const token = getToken();
-    const payload = getPayloadData(token);
-    getTokenExpirationDate(payload.exp);
+    const accessToken = localStorage.getItem('accessToken');
 
-    if (token) {
+    if (accessToken) {
+        const payload = getPayloadData(accessToken);
+        getTokenExpirationDate(payload.exp);
         //토큰 있는 경우: 로그인 상태
         const tokenExpiration = payload.exp * 1000; //밀리초 단위로 변환
         const currentTime = Date.now();
@@ -76,7 +75,8 @@ function checkTokenExpiration() {
             console.log("토큰 만료")
             //토큰 만료된 경우: 로그아웃 처리
             localStorage.removeItem('accessToken');
-            refreshAccessToken();   //access token 재발급 OR refresh token 삭제
+            //access token 재발급 OR refresh token 삭제
+            refreshAccessToken();
         }
     }
 }
