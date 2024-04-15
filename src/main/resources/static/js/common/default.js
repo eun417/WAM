144cf2a28a6741674b2cdaacadce254cd05c8adf
eun417 <eunahchung0417@gmail.com>
2024-04-15 23:59:32 +0900

    feat: axios interceptors로 Refresh 요청

--- src/main/resources/static/js/common/default.js
+++ src/main/resources/static/js/common/default.js
@@ -23,3 +23,54 @@ api.interceptors.request.use(
     return Promise.reject(error);
   }
 );
+
+/*Access Token 재발급*/
+api.interceptors.response.use(
+  function (response) {
+    console.log(response);
+    return response;
+  },
+  async function (error) {
+    //오류 요청을 보내기전 수행할 일
+    const msg = error.response.data.message;
+    const status = error.response.status;
+    console.log('msg', msg);
+    console.log('status', status);
+
+    if (status === 401) {
+        if (msg === "Unauthorized") {
+            const refreshToken = localStorage.getItem('refreshToken');
+            if (!refreshToken) {
+                return Promise.reject('Refresh token을 찾을 수 없습니다.');
+            }
+
+            //TokenRequestDto 객체 생성
+            const tokenReq = {
+                accessToken: localStorage.getItem('accessToken'),
+                refreshToken: refreshToken
+            };
+
+            await api.post('/auth/refresh', tokenReq)
+                .then(function(response) {
+                    console.log("토큰 재발급 성공")
+                    //새로운 access token 발급
+                    const accessToken = response.data.accessToken;
+                    localStorage.setItem('accessToken', accessToken);
+                    error.config.headers["Authorization"] = `Bearer ${accessToken}`;
+                    return api(error.config);
+                })
+                .catch(function(refreshError) {
+                    console.error("토큰 재발급 오류:", refreshError);
+                    return Promise.reject(refreshError);
+                });
+        } else if (msg === "리프레시 토큰이 만료되었습니다.") {
+            localStorage.clear();
+            window.location.href = "/auth/login";
+            alert("토큰이 만료되어 자동으로 로그아웃 되었습니다.");
+        }
+    } else if (status == 400) {
+        alert(msg);
+    }
+    return Promise.reject(error);
+  }
+);
